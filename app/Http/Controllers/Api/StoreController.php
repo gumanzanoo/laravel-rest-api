095b271bc3b30c32ed7160157221267647e77a18
root <root@guManzano_dev>
2023-01-27 23:14:42 -0300

    chore

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -11,7 +11,7 @@ class StoreController extends Controller
 {
     public function index()
     {
-        return Store::paginate(5);
+        return Store::paginate(10);
     }
 
     public function store(CreateStore $request)
